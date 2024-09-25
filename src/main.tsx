0f3f268524c5ed5729110037f8f1b1894a25edda
Oliwier Matus <matusoliwier@gmail.com>
2024-09-25 18:24:37 +0200

    feat: add theme file

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 
 import App from './App';
+import { theme } from './assets/styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
